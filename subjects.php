0c6d97e19c00b0339401b2296ff2407342bb1f8b
Lukas Kaufmann <travianluk@gmail.com>
2021-10-20 11:53:30 +0200

    add subjects page, update darkmode css

---
+++ subjects.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Předměty</title>
+    <link rel="stylesheet" href="css/base.css">
+</head>
+<body>
+<?php include 'base_format.html'?>
+</body>
+</html>
