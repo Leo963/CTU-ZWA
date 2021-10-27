ad07ae0e014dac11da72949f2287a72f7c33bb1a
Lukas Kaufmann <travianluk@gmail.com>
2021-10-27 11:54:46 +0200

    Add grades page, updated dark mode

---
+++ grades.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Známky</title>
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/grades.css">
+</head>
+<body>
+
+<?php include 'header.html' ?>
+<div class="content">
+    <?php include 'nav.html' ?>
+    <section class="main">
+        <div class="grades">
+            <table>
+                <thead>
+                <tr>
+                    <th>Předmět</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <th>Final</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th>Předmět 1</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Předmět 2</th>
+                    <td>C</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Předmět 3</th>
+                    <td>B</td>
+                    <td>A</td>
+                    <td>A</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Předmět 4</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Předmět 5</th>
+                    <td>A</td>
+                    <td>B</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Předmět 6</th>
+                    <td>C</td>
+                    <td>D</td>
+                    <td>A</td>
+                    <td>E</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+</div>
+</body>
+</html>
--- subjects.php
+++ subjects.php
@@ -4,16 +4,62 @@
     <meta charset="UTF-8">
     <title>Předměty</title>
     <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/subjects.css">
 </head>
 <body>
-<?php include 'header.html'?>
+<?php include 'header.html' ?>
 <div class="content">
 
-    <?php include 'nav.html'?>
+    <?php include 'nav.html' ?>
     <section class="main">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae libero error placeat molestias consequuntur molestiae. Reiciendis architecto rem officia assumenda natus totam ducimus cum aspernatur possimus! Harum, voluptatum! Expedita, obcaecati?
-            Eligendi suscipit quae corporis nesciunt, delectus eaque sapiente amet. Velit totam vel inventore, temporibus soluta, tempora rerum eveniet aliquid doloremque repudiandae minima consequatur voluptatum a officiis, voluptatibus iusto ut magni.
-            Nam nostrum inventore magnam perspiciatis ad molestiae recusandae, in, quos aperiam eligendi aut laudantium ea voluptatibus. Molestiae praesentium recusandae ullam id nam. Atque explicabo ducimus nesciunt error et ea eaque!</p>
+        <h2>Dostupné předměty</h2>
+        <div class="sub-list">
+            <section class="list">
+                <article class="subject">
+                    <h3 class="code">Kód</h3>
+                    <h3 class="name">Název</h3>
+                    <button class="detail" onclick="showDetail()">Detail</button>
+                </article>
+                <article class="subject">
+                    <h3 class="code">Kód</h3>
+                    <h3 class="name">Název</h3>
+                    <button class="detail" onclick="showDetail()">Detail</button>
+                </article>
+                <article class="subject">
+                    <h3 class="code">Kód</h3>
+                    <h3 class="name">Název</h3>
+                    <button class="detail" onclick="showDetail()">Detail</button>
+                </article>
+                <article class="subject">
+                    <h3 class="code">Kód</h3>
+                    <h3 class="name">Název</h3>
+                    <button class="detail" onclick="showDetail()">Detail</button>
+                </article>
+                <article class="subject">
+                    <h3 class="code">Kód</h3>
+                    <h3 class="name">Název</h3>
+                    <button class="detail" onclick="showDetail()">Detail</button>
+                </article>
+                <article class="subject">
+                    <h3 class="code">Kód</h3>
+                    <h3 class="name">Název</h3>
+                    <button class="detail" onclick="showDetail()">Detail</button>
+                </article>
+            </section>
+            <nav>
+                <a href="" id="left">
+                    <img src="assets/leftarrow.svg" alt="Previous page in list">
+                </a>
+                <a href="">1</a>
+                <a href="">2</a>
+                <a href="">3</a>
+                <a href="">4</a>
+                <a href="">5</a>
+                <a href="" id="right">
+                    <img src="assets/rightarrow.svg" alt="Next page in list">
+                </a>
+            </nav>
+        </div>
     </section>
 </div>
 </body>
