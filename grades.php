53827d516667580dc3bdc83ce23601de3279302b
Lukas Kaufmann <travianluk@gmail.com>
2021-10-28 18:12:49 +0200

    html changes

--- grades.php
+++ grades.php
@@ -2,9 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Známky</title>
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/grades.css">
@@ -15,6 +12,7 @@
 <div class="content">
     <?php include 'nav.html' ?>
     <section class="main">
+        <h2>Známky</h2>
         <div class="grades">
             <table>
                 <thead>
