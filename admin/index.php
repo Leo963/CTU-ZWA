fcb1434beba5289c93ddb2767b5e58984022eb70
Lukas Kaufmann <travianluk@gmail.com>
2021-11-10 13:33:56 +0100

    admin setup

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Administrace</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/admin.css">
+    <script src="../js/admin.js"></script>
+</head>
+<body>
+<?php include '../header.html' ?>
+<section class="main">
+    <h2>Administrační rozcestník</h2>
+    <div class="tabs">
+        <button class="tablink active" id="students">Studenti</button>
+        <button class="tablink" id="subjects">Předměty</button>
+    </div>
+    <div class="tab" id="students">
+
+    </div>
+    <div class="tab" id="subjects">
+
+    </div>
+</section>
+
+</body>
+</html>
